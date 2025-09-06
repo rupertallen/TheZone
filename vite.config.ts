24a34d898277ab2705660e363bfc93e4636740e1
Rupert Allen <rupert.allen@gmail.com>
2025-09-06 20:18:05 +0100

    adding vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+  plugins: [react()];
+  base: '/TheZone/';  
+  const env = loadEnv(mode, '.', '');
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
